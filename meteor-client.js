b41075070bb27fd77327e2cb931cdd5f64b25263
Adam Faur <adam.faur@gmail.com>
2015-09-27 15:20:17 -0500

    Minimize includes.

--- meteor-client.js
+++ meteor-client.js
@@ -1,16 +1,7 @@
 module.exports = function(jQuery,underscore) {
   var Meteor = require("meteor-core")(underscore);
-  require("meteor-base64")(Meteor);
-  require("meteor-ejson-safe")(Meteor);
-  require("meteor-random-window-crypto")(Meteor);
-  require("meteor-id-map")(Meteor);
-  require("meteor-ordered-dict")(Meteor);
   require("meteor-tracker")(Meteor);
-  require("meteor-observe-sequence")(Meteor);
-  require("meteor-htmljs")(Meteor);
-  require("meteor-html-tools")(Meteor);
   require("meteor-reactive-var")(Meteor);
-  require("meteor-reactive-object-map")(Meteor);
   require("meteor-blaze")(Meteor,jQuery);
   require("meteor-blaze-tools")(Meteor);
   require("meteor-templating")(Meteor);
